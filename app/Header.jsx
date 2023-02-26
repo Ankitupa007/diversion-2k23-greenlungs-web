cba952f548750f1def4a760c567187e26488668b
Ankit Upadhyay <ankit.upa007@gmail.com>
2023-02-26 07:50:06 +0530

    header changes

--- app/Header.jsx
+++ app/Header.jsx
@@ -9,7 +9,7 @@ export default function Header() {
         <nav className="w-full bg-white backdrop-blur-sm shadow z-10 fixed">
             <div className="flex justify-between px-10 mx-auto py-4 lg:max-w-7xl md:px-8">
                 <div>
-                    <div className="flex items-center justify-between py-3 md:py-5 rounded-lg">
+                    <div className="flex items-center justify-between py-3 md:py-5 rounded-lg mt-2">
                         <Link href="/">
                             <Image src={"/logo/logo.svg"} className="max-w-full h-auto rounded-lg" width={150} height={60} />
                         </Link>
@@ -19,11 +19,8 @@ export default function Header() {
                 <div>
 
                     <div class="flex items-center justify-between py-3 md:py-5">
-                        <Link href="/" class="bg-gray-100 inline-flex py-3 px-5 rounded-lg items-center hover:bg-gray-200 focus:outline-none">
-                            <FaGooglePlay />
-                            <span class="ml-4 flex items-start flex-col leading-none">
-                                <span class="title-font font-medium">Google Play</span>
-                            </span>
+                        <Link href="/" class=" inline-flex py-3 px-5 rounded-lg items-center hover:text-green-500 focus:outline-none">
+                            <FaGooglePlay className="text-2xl" />
                         </Link>
 
                     </div>
